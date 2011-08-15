33678db48da59b5d81b9f24f0624fe79a71b8a44
Sam King <kingst@illinois.edu>
2011-08-15 10:27:41 -0500

    fixed run script

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 case $1 in
     busybox )
-        qemu-system-x86_64 -m 512 -nographic -smp 8 -kernel ../linux-3.0/arch/x86/boot/bzImage -append "root=/dev/sda rw init=/sbin/init norandmaps console=ttyS0,115200 debug" -hda rootfs.ext3 -net nic,model=e1000 -net user
+        if [ -f ../disk_images/rootfs.ext3.gz ]; then
+            if [ ! -f ../disk_images/rootfs.ext3 ]; then
+                echo "uncompressing disk image...."
+                gunzip < ../disk_images/rootfs.ext3.gz > ../disk_images/rootfs.ext3
+            fi
+        fi
+        qemu-system-x86_64 -m 512 -nographic -smp 8 -kernel ../linux-3.0/arch/x86/boot/bzImage -append "root=/dev/sda rw init=/sbin/init norandmaps console=ttyS0,115200 debug" -hda ../disk_images/rootfs.ext3 -net nic,model=e1000 -net user
         ;;
     ubuntu )
         qemu-system-x86_64 -m 512 -smp 8 -hda ../disk_images/ubuntu-10.04-x86_64.qcow2 -nographic -kernel ../linux-3.0/arch/x86/boot/bzImage -append "root=/dev/sda1 rw init=/sbin/init norandmaps console=ttyS0,115200 debug" -net nic,model=e1000 -net user
